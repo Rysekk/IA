9be1324c36d2b88da5ab6b2876f24943ba4fde24
Trithuan <trithuan91@gmail.com>
2019-05-26 19:50:28 +0200

    diminution vitesse et plus petite voiture

--- test.py
+++ test.py
@@ -4,15 +4,18 @@ pygame.init()
 WIDTH = 1000
 HEIGHT = 600
 screen = pygame.display.set_mode((WIDTH,HEIGHT))
+R = 255
+V = 0
+B = 0
 RED = (255,0,0)
 BLUE = (0,0,255)
 BLACK = (0,0,0)
 BG_COLOR = BLACK
 voiture_pos = [100, 100]
-voiture_largeur = 50
-voiture_longueur = 100
+voiture_largeur = 25
+voiture_longueur = 50
 game_over = False
-speed = 6
+speed = 3
 voiture_x = WIDTH/2
 voiture_y = HEIGHT/2
 a_droite = False
@@ -31,7 +34,8 @@ voiture.fill(BLACK)
 # frame suivante avec rotation 
 voiture_tourne = voiture.copy()
 # déssine la voiture
-pygame.draw.rect(voiture_tourne,RED, (1, 1, voiture_largeur,voiture_longueur))
+RAINBOW = (R,V,B)
+pygame.draw.rect(voiture_tourne,RAINBOW, (1, 1, voiture_largeur,voiture_longueur))
 
 
 
@@ -66,7 +70,8 @@ while not game_over:
 
 
 	# dessine la voiture
-	pygame.draw.rect(voiture,RED, (1, 1, voiture_largeur,voiture_longueur))
+	RAINBOW = (R,V,B)
+	pygame.draw.rect(voiture,RAINBOW, (1, 1, voiture_largeur,voiture_longueur))
 	# efface tout
 	screen.fill(BG_COLOR)
 	# pygame.draw.rect(screen, RED, (voiture_x, voiture_y, voiture_largeur, voiture_longueur))
@@ -83,18 +88,23 @@ while not game_over:
 
 	# action selon les touches appuiyé
 	if a_droite is True:
-		angle = (angle-3)%360
+		angle = (angle-2)%360
 		voiture_tourne = pygame.transform.rotate(voiture, -angle-90)
 	if a_gauche is True:
-		angle = (angle+3)%360
+		angle = (angle+2)%360
 		voiture_tourne = pygame.transform.rotate(voiture, -angle-90)
 	if en_haut is True:
 		voiture_y += speed * sin(angle / (180 / 3.14))
 		voiture_x += speed * cos(angle / (180 / 3.14))
+		voiture_tourne = pygame.transform.rotate(voiture, -angle-90)
 	if en_bas is True:
 		voiture_y += speed
+		voiture_tourne = pygame.transform.rotate(voiture, -angle-90)
 
 	if  WIDTH < voiture_x or voiture_x < 0 or HEIGHT < voiture_y or voiture_y < 0:
+		# R = (R+54)%255
+		# V = (V+81)%255
+		# B = (B+103)%255
 		print("dehors")
 		voiture_x = WIDTH/2
 		voiture_y = HEIGHT/2
