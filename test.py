983453b63872b8269e0fe166c97f3c782c77538d
Trithuan <trithuan91@gmail.com>
2019-05-29 23:19:47 +0200

    correction manette

--- test.py
+++ test.py
@@ -116,8 +116,9 @@ pygame.draw.lines(screen, BLUE, False, liste_point_interieur, trait_large)
 
 
 pygame.joystick.init()
-stick = [pygame.joystick.Joystick(x) for x in range(pygame.joystick.get_count())]
-stick[0].init()
+stick = [pygame.joystick.Joystick(x).init() for x in range(pygame.joystick.get_count())]
+# if stick
+# stick[0].init()
 
 while not game_over:
 	#regarde les touches qu'on appui
